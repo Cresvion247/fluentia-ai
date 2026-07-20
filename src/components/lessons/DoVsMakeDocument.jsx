985385944ee0cb2f859e840b7d3f5cb26609fde9
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 13:53:25 +0000

    File changes

---
+++ src/components/lessons/DoVsMakeDocument.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DocxTableDocument from "@/components/lessons/DocxTableDocument";
+
+const sourceUrl = "https://media.base44.com/files/public/6a5d04483fe35abf39399c85/a09200e0b_DIFFERENCEBETWEENDOANDMAKE.docx";
+
+export default function DoVsMakeDocument({ onSelect }) {
+  return <DocxTableDocument sourceUrl={sourceUrl} onSelect={onSelect} />;
+}
