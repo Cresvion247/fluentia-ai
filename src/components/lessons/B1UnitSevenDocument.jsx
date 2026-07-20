5165ab716a4cb3fbc80da99d3632a10439ac68f9
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 09:12:57 +0000

    File changes

---
+++ src/components/lessons/B1UnitSevenDocument.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DocxTableDocument from "@/components/lessons/DocxTableDocument";
+
+export default function B1UnitSevenDocument({ onSelect }) {
+  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/68b92b3ce_B1DestinationUnit7Questions.docx" onSelect={onSelect} fullWidth={false} />;
+}
