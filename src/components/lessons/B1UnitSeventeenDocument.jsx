9884c0581673eace2d53ee618b1d0f6965f6631f
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 09:44:30 +0000

    File changes

--- src/components/lessons/B1UnitSeventeenDocument.jsx
+++ src/components/lessons/B1UnitSeventeenDocument.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import DocxTableDocument from "@/components/lessons/DocxTableDocument";
 
 export default function B1UnitSeventeenDocument({ onSelect }) {
-  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/f28b2ae89_B1DestinationUnit17Questions.docx" onSelect={onSelect} fullWidth={false} />;
+  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/f28b2ae89_B1DestinationUnit17Questions.docx" onSelect={onSelect} fullWidth={false} headingOverride="B1 Destination Unit 17 Questions" />;
 }
