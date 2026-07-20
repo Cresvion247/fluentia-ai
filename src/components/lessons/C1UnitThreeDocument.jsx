11d9cbeefae9a7a1474c85b2191ad421f41eb353
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 10:44:47 +0000

    File changes

---
+++ src/components/lessons/C1UnitThreeDocument.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DocxTableDocument from "@/components/lessons/DocxTableDocument";
+
+export default function C1UnitThreeDocument({ onSelect }) {
+  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/3f89cf673_DestinationC1Unit3QA.docx" onSelect={onSelect} fullWidth={false} />;
+}
