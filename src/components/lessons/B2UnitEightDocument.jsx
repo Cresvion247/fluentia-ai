a18cc25ca8cd433283975483c5c121eea619e98d
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 10:29:32 +0000

    File changes

---
+++ src/components/lessons/B2UnitEightDocument.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DocxTableDocument from "@/components/lessons/DocxTableDocument";
+
+export default function B2UnitEightDocument({ onSelect }) {
+  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/6d9a860db_B2DestinationUnit8QA.docx" onSelect={onSelect} fullWidth={false} />;
+}
