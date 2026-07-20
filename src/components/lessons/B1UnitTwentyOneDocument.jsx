a7187c69fd8094042cab6b4841cf7e3d841e11da
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 09:49:56 +0000

    File changes

---
+++ src/components/lessons/B1UnitTwentyOneDocument.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DocxTableDocument from "@/components/lessons/DocxTableDocument";
+
+export default function B1UnitTwentyOneDocument({ onSelect }) {
+  return <DocxTableDocument sourceUrl="https://media.base44.com/files/public/6a5d04483fe35abf39399c85/fc2a465c6_B1DestinationUnit21Questions.docx" onSelect={onSelect} fullWidth={false} headingOverride="B1 Destination Unit 21 Questions" />;
+}
