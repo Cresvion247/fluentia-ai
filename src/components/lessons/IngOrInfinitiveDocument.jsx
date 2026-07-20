bc2b163469392dd9d5b130d9adc39e8d05e09892
Kamran Shahzad <kamskillz1@gmail.com>
2026-07-20 08:45:11 +0000

    File changes

--- src/components/lessons/IngOrInfinitiveDocument.jsx
+++ src/components/lessons/IngOrInfinitiveDocument.jsx
@@ -4,5 +4,5 @@ import DocxTableDocument from "@/components/lessons/DocxTableDocument";
 const sourceUrl = "https://media.base44.com/files/public/6a5d04483fe35abf39399c85/b6743ff4d_ingorinfQA.docx";
 
 export default function IngOrInfinitiveDocument({ onSelect }) {
-  return <DocxTableDocument sourceUrl={sourceUrl} onSelect={onSelect} />;
+  return <DocxTableDocument sourceUrl={sourceUrl} onSelect={onSelect} fullWidth />;
 }
